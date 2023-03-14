ebb5dd214e0ecd11a69c6ece9461b45508591c24
soucs <soumya.sumswap@gmail.com>
2023-03-14 22:16:44 +0530

    Img address change

--- Q1/cv_assignment.py
+++ Q1/cv_assignment.py
@@ -9,15 +9,15 @@ def resize_img(img, scale=0.15):
     return cv.resize(img,dim,interpolation=cv.INTER_AREA)
 
 # Reading Original and Edited images
-org_img = resize_img(cv.imread(r'/home/soucs/Python/data/Sunset_org1.jpg'))
-edit_img = resize_img(cv.imread(r'/home/soucs/Python/data/Sunset_edit.jpg'))
+org_img = resize_img(cv.imread(r'/home/soucs/Python/computer-vision-hands-on/Q1/dataset/Sunset_org.jpg'))
+edit_img = resize_img(cv.imread(r'/home/soucs/Python/computer-vision-hands-on/Q1/dataset/Sunset_edit.jpg'))
 
 # View Images
 cv.imshow('Edit',edit_img)
 cv.imshow('Org',org_img)
 
 # Pixel difference image
-difference = edit_img-org_img
+difference = np.subtract(edit_img, org_img)
 
 # Show difference
 cv.imshow('Diff',difference)
