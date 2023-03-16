80384c34ad7801072c99a479d89c5bb5728ef841
soucs <soumya.sumswap@gmail.com>
2023-03-16 07:43:13 +0530

    Working Code

--- Q5/video_bkg.py
+++ Q5/video_bkg.py
@@ -3,15 +3,37 @@
 import cv2 as cv
 from external.SelfiSegmentationModule import SelfiSegmentation
 
-vid = cv.VideoCapture(0)
-vid.set(3, 640) # width
-vid.set(4, 480) # height
-bg_img = cv.imread('/home/soucs/Python/computer-vision-hands-on/Q5/dataset/milky-way.jpg')
+WIDTH, HEIGHT = (640, 480)
+
+def resize_img(img, width=WIDTH, height=HEIGHT ):
+    dim = (640, 480)
+    return cv.resize(img,dim,interpolation=cv.INTER_AREA)
+
+# vid = cv.VideoCapture(0)
+# vid.set(3, WIDTH) # width
+# vid.set(4, HEIGHT) # height
+
+# bg_img = resize_img(cv.imread('/home/soucs/Python/computer-vision-hands-on/Q5/dataset/milky-way.jpg'))
+# seg = SelfiSegmentation()
+
+# while True:
+#     _, video = vid.read()
+#     vid_rmbg = seg.removeBG(video, bg_img, threshold=0.85)
+#     cv.imshow("Video", vid_rmbg)
+#     if cv.waitKey(1)==ord('x'):
+#         break
+
+bg_img = resize_img(cv.imread('/home/soucs/Python/computer-vision-hands-on/Q5/dataset/milky-way.jpg'))
 seg = SelfiSegmentation()
 
+vid = cv.VideoCapture(r'/home/soucs/Python/computer-vision-hands-on/output.mp4')
 while True:
-    _, video = vid.read()
-    vid_rmbg = seg.removeBG(video, bg_img, threshold=0.8)
-    cv.imshow("Video", video)
-    if cv.waitKey(1)==ord('x'):
+    isTrue, frame = vid.read()
+    frame = resize_img(frame)
+    frame_rmbg = seg.removeBG(frame, bg_img, threshold=0.8)
+    cv.imshow('Video', frame_rmbg)
+    cv.waitKey(50)
+    if cv.waitKey(1) == ord('x'):
         break
+
+cv.destroyAllWindows()
