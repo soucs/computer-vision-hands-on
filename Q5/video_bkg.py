c0c83e9e309838666d998bfd72b4c00f3e829784
soucs <soumya.sumswap@gmail.com>
2023-03-31 19:16:26 +0530

    Complete Code

--- Q5/video_bkg.py
+++ Q5/video_bkg.py
@@ -5,28 +5,14 @@ from external.SelfiSegmentationModule import SelfiSegmentation
 
 WIDTH, HEIGHT = (640, 480)
 
-def resize_img(img, width=WIDTH, height=HEIGHT ):
+def resize_img(img, width=WIDTH, height=HEIGHT):
     dim = (640, 480)
     return cv.resize(img,dim,interpolation=cv.INTER_AREA)
-
-# vid = cv.VideoCapture(0)
-# vid.set(3, WIDTH) # width
-# vid.set(4, HEIGHT) # height
-
-# bg_img = resize_img(cv.imread('/home/soucs/Python/computer-vision-hands-on/Q5/dataset/milky-way.jpg'))
-# seg = SelfiSegmentation()
-
-# while True:
-#     _, video = vid.read()
-#     vid_rmbg = seg.removeBG(video, bg_img, threshold=0.85)
-#     cv.imshow("Video", vid_rmbg)
-#     if cv.waitKey(1)==ord('x'):
-#         break
 
 bg_img = resize_img(cv.imread('/home/soucs/Python/computer-vision-hands-on/Q5/dataset/milky-way.jpg'))
 seg = SelfiSegmentation()
 
-vid = cv.VideoCapture(r'/home/soucs/Python/computer-vision-hands-on/output.mp4')
+vid = cv.VideoCapture(r'/home/soucs/Python/computer-vision-hands-on/Q5/dataset/output.mp4')
 while True:
     isTrue, frame = vid.read()
     frame = resize_img(frame)
